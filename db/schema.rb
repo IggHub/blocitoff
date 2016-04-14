ab4e9a96b7c5e4f7030bdff11b7cc7e69354d024
Iggy <igoririanto@rocketmail.com>
2016-04-14 11:32:22 -0700

    Email confirmable works on localhost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407192406) do
+ActiveRecord::Schema.define(version: 20160413201123) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -35,9 +35,16 @@ ActiveRecord::Schema.define(version: 20160407192406) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "username"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
