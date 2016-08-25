a2d8bcd5556d932a635dbcdbbfd8c4b8d87a2276
Iggy <igoririanto@rocketmail.com>
2016-08-25 16:56:22 -0700

    edit layout

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,15 +4,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-
-
     @user = User.find(params[:user_id])
     @item = current_user.items.create(item_params)
-
-    #@item = Item.new(item_params)
-    #@item.user = current_user #need to define current_user(?)
-#do I use Item.new, or @item = current_user.items.create(item_params) - or something like that?
-    #need to check code below:
     if @item.save
       flash[:notice] = "Item was created!"
       redirect_to @user
@@ -20,7 +13,6 @@ class ItemsController < ApplicationController
       flash.now[:alert] = "Alert!!! Unable to create item"
       redirect_to @user
     end
-
   end
 
   def update
@@ -30,7 +22,6 @@ class ItemsController < ApplicationController
     @user = User.find(params[:user_id])
     @item = Item.find(params[:id])
     @item.destroy
-
     if @item.destroy
       flash[:notice] = "Item was checked! Good job!"
       redirect_to @user
@@ -48,11 +39,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name)
   end
-
-
-  respond_to do |format|
-   format.html
-   format.js
-  end
-
+#  respond_to do |format|
+#   format.html
+#   format.js
+#  end
 end
